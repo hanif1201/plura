6458fa94c46f4ebd231844befb1bc8d76d71f2d1
Hanif1201 <adeyemihanif@gmail.com>
2024-05-14 15:59:08 +0100

    landingpage updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import { ThemeProvider } from "../../providers/theme-provider";
+import { ThemeProvider } from "../providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
